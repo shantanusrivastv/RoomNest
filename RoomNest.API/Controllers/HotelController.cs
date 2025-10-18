3e8173c0c31462eb194d58119567e762b4f15349
Kumar Shantanu <shantanusrivastv@gmail.com>
2025-10-18 19:21:50 +0530

    Add project files.

---
+++ RoomNest.API/Controllers/HotelController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoomNest.Model;
+
+namespace RoomNest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        // In-memory hotel list for demonstration
+        private static readonly List<Hotel> Hotels = new List<Hotel>
+        {
+            new Hotel { Id = 1, Name = "Hotel Sunshine" },
+            new Hotel { Id = 2, Name = "Mountain View Inn" }
+        };
+
+        [HttpGet("{name}")]
+        public ActionResult<Hotel> GetHotelByName(string name)
+        {
+            var hotel = Hotels.Find(h => h.Name.Contains(name, StringComparison.InvariantCultureIgnoreCase));
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return Ok(hotel);
+        }
+    }
+}
---
+++ RoomNest.Model/Hotel.cs
@@ -0,0 +1,8 @@
+﻿namespace RoomNest.Model
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
